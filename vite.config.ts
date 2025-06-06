48cbd4e19a906209cee43eccdbac3b2fcc92e405
Anderson Chiesa <80640593+Andchiesa@users.noreply.github.com>
2025-06-06 15:58:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,21 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  // ADICIONE ESTA LINHA
-  base: "/business-insight-glow-3b42ee0f/",
 
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+export default defineConfig(({ mode }) => ({
+
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 }));
